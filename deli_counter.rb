b8fe2d50b1964afefc0287c70515ee2d6196a588
Adi Skop <adiskop@gmail.com>
2020-07-03 07:58:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,12 +6,13 @@ def line(deli)
   else
   current line="The line is currently:"
   deli.each.with_index(1) do |person,i|
-  current_line << "#{i}. {person}"
+  current_line << " #{i}. {person}"
 end
   puts current_line
 
-end
+  end
 end
 
 
 #do |value, index| end
+
